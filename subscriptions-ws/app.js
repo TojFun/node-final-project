8e27f8d8858b7403c49120e49cc09799e9f8fec0
Ofek Jacobi <ofek.jacobi@gmail.com>
2021-06-26 02:38:46 +0300

    Setting Routes

--- subscriptions-ws/app.js
+++ subscriptions-ws/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 
-const testRoute = require("./routes/test");
+const membersRoute = require("./routes/members");
+const moviesRoute = require("./routes/movies");
+const subscriptionsRoute = require("./routes/subscriptions");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 require("./configs/dataBase");
 
-app.use("/test", testRoute);
+app.use("/members", membersRoute);
+app.use("/movies", moviesRoute);
+app.use("/subscriptions", subscriptionsRoute);
 
 app.listen(8000);
